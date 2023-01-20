5a9bbfd31e13e39152bd1795755500f90671e7d8
Nazim Bayraktar <nab1079@hotmail.com>
2023-01-20 14:05:49 +0000

    Added some html tags using JavaScript, and styled some elements in style.css

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 import {
   menuArray
 } from '/data.js'
+
+
+function getMenuHtml() {
+  let menuHtml = ''
+  menuArray.forEach(function (menu) {
+    menuHtml += `
+    <div class="item-wraper">
+      <div class="section-1">
+        <div class="item-emoji">${menu.emoji}</div>
+        <div class="text__wraper">
+          <p class="text item-title">${menu.name}</p>
+          <p class="text item-description">${menu.ingredients}</p>
+          <p class="text item-price">£ ${menu.price}</p>
+        </div>
+      </div> 
+
+      <div class="section-2">
+        <div class="add-btn">
+          +
+        </div>
+      </div> 
+
+    </div>
+    `
+  })
+  return menuHtml
+}
+
+function render() {
+  document.getElementById('main-section').innerHTML = getMenuHtml()
+}
+
+render()
