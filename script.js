64c51da1dac1452f1845638af436513a75bfe25b
Nazim Bayraktar <nab1079@hotmail.com>
2023-01-21 10:45:39 +0000

    Work donde for order section. Added elements for order section with javascript in script.js file, and worked on style in style.css

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ import {
   menuArray
 } from '/data.js'
 
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.add) {
+    renderOrder()
+  }
+})
+
+function renderOrder() {
+  let orderHtml = ''
+
+
+}
 
 function getMenuHtml() {
   let menuHtml = ''
   menuArray.forEach(function (menu) {
     menuHtml += `
     <div class="item-wraper">
+
       <div class="section-1">
         <div class="item-emoji">${menu.emoji}</div>
         <div class="text__wraper">
@@ -18,19 +30,55 @@ function getMenuHtml() {
       </div> 
 
       <div class="section-2">
-        <div class="add-btn">
+        <div class="add-btn" data-add='${menu.id}'>
           +
         </div>
       </div> 
-
+      
     </div>
+  
     `
   })
   return menuHtml
 }
+let orderHtml = ''
+orderHtml += `
+<p class="text item-title order-title">Your order</p>
+<div class="order-wraper">
+  <div class="order">
+    <p class="text item-title order-text">Pizza</p>
+    <p class="text item-title remove">remove</p>
+  </div>
+  <p class="text item-price order-price">£0</p>
+</div>
+
+<div class="order-wraper">
+  <div class="order">
+    <p class="text item-title order-text">Hamburger</p> 
+    <p class ="text item-title remove">remove</p>
+  </div> 
+  <p class="text item-price order-price">£0</p>
+</div>
+<div class="order-wraper">
+  <div class="order">
+    <p class="text item-title order-text">Beer</p>
+    <p class="text item-title remove">remove</p>
+  </div>
+  <p class="text item-price order-price">£0</p>
+</div>
+<div class="order-divider"></div>
+<div class="order-wraper">
+  <p class="text item-title order-text">Total prise:</p>
+  <p class="text item-price order-price">£0</p>
+</div>
+<button class="complete-btn" id="complete-btn">Complete order</button>
+`
+
+
 
 function render() {
   document.getElementById('main-section').innerHTML = getMenuHtml()
+  document.querySelector('.order-section').innerHTML = orderHtml
 }
 
 render()
